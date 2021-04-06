81ad2d68bb8f0faee6e49890a93aa5076d71c49d
Leandro Figueiredo <leandro.figueiredo@savn.com.br>
2021-04-06 18:09:30 -0300

    fork savn

--- examples/ping/ping.go
+++ examples/ping/ping.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"time"
 
-	"github.com/AlexMarco7/aclow"
+	"github.com/leandrosavn/aclow"
 )
 
 type Ping struct{}
--- examples/ping/starter.go
+++ examples/ping/starter.go
@@ -4,7 +4,7 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/AlexMarco7/aclow"
+	"github.com/leandrosavn/aclow"
 )
 
 func main() {
